fc9c6b9b8cc0ad2b3c28cdfcf1437c5df52989bc
ridvan <sevimrid@students.zhaw.ch>
2022-04-10 18:08:50 +0200

    Implementation Local available disk space #23

--- src/main/java/ch/psc/domain/storage/service/LocalStorage.java
+++ src/main/java/ch/psc/domain/storage/service/LocalStorage.java
@@ -4,8 +4,17 @@ import ch.psc.datasource.datastructure.Tree;
 import ch.psc.domain.file.PscFile;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+
+/**
+ * Beinhaltet Methoden um loklae File Operationen durchzuführen
+ *
+ * @author sevimrid, walchr01
+ */
 public class LocalStorage implements FileStorage {
   
   private String path;
@@ -23,10 +32,34 @@ public class LocalStorage implements FileStorage {
     return null;
   }
 
+  /**
+   * Gibt einen double Wert zurueck mit dem freien Speicher in GB
+   *
+   * @return Groese des freien Speichers im System in GB
+   */
   @Override
-  public double getAvailableStorageSpace() {
-    // TODO Auto-generated method stub
-    return 0;
+  public double getAvailableStorageSpace(){
+    double size = 0;
+    ExecutorService executorService = Executors.newSingleThreadExecutor();
+    long startTime = System.currentTimeMillis();
+    Future<Double> future = executorService.submit(() -> {
+      double result = 0;
+      java.io.File[] paths = java.io.File.listRoots();
+      for(java.io.File path: paths){
+        result += new java.io.File(path.toString()).getFreeSpace() / (1024.0 * 1024 * 1024);
+      }
+      return result;
+    });
+    while(!future.isDone()){
+      double elapsedTimeInMillis = System.currentTimeMillis() - startTime;
+    }
+    try {
+      size = future.get();
+    }catch (InterruptedException | ExecutionException  e) {
+      e.printStackTrace();
+    }
+    executorService.shutdown();
+    return size;
   }
   
   @Override
