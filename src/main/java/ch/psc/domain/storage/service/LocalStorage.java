8a0ac434871de5fdaded3b8d643917bddfb36dd5
ridvan-sevimli <68498010+ridvan-sevimli@users.noreply.github.com>
2022-04-20 10:38:16 +0200

    Implementation Local available disk space #23 (#26)
    * Implementation Local available disk space #23
    
    * Aufteilung Controller und Views und Hiflsklassen (#31)
    
    * Aufteilung Controller und Views und Hifls Klassen
    
    * Deletet Controller class
    
    Co-authored-by: ridvan <sevimrid@students.zhaw.ch>
    
    * Feature/19 dropbox and Feature/22 register screen (#35)
    
    * #19 created dropbox connections
    added json read/writer
    #22 created register form
    
    * updated screens to controlled screens
    
    * #19 implemented test class
    added json read/writer tests
    #22 updated signup form to controlled screen
    
    * Changed naming convention of classes to avoid conflicts with java standard library (#37)
    
    Co-authored-by: sevimrid <sevimrid@students.zhaw.ch>
    
    * Implementation Local available disk space #23
    
    * refactoring java.io.File to File implemented
    
    * changed comments in to english
    
    Co-authored-by: ridvan <sevimrid@students.zhaw.ch>
    Co-authored-by: Sandro Guerotto <s.guerotto@gmail.com>

--- src/main/java/ch/psc/domain/storage/service/LocalStorage.java
+++ src/main/java/ch/psc/domain/storage/service/LocalStorage.java
@@ -4,8 +4,18 @@ import ch.psc.datasource.datastructure.Tree;
 import ch.psc.domain.file.PscFile;
 
 import java.util.List;
+import java.io.File;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+
+/**
+ * Contains methods to perform local file operations
+ *
+ * @author sevimrid, walchr01
+ */
 public class LocalStorage implements FileStorage {
   
   private String path;
@@ -23,10 +33,34 @@ public class LocalStorage implements FileStorage {
     return null;
   }
 
+  /**
+   * Returns a double value with the free memory in GB
+   *
+   * @return Amount of free space in the system in GB
+   */
   @Override
-  public double getAvailableStorageSpace() {
-    // TODO Auto-generated method stub
-    return 0;
+  public double getAvailableStorageSpace(){
+    double size = 0;
+    ExecutorService executorService = Executors.newSingleThreadExecutor();
+    long startTime = System.currentTimeMillis();
+    Future<Double> future = executorService.submit(() -> {
+      double result = 0;
+      File[] paths = File.listRoots();
+      for(File path: paths){
+        result += new File(path.toString()).getFreeSpace() / (1024.0 * 1024 * 1024);
+      }
+      return result;
+    });
+    while(!future.isDone()){
+      double elapsedTimeInMillis = System.currentTimeMillis() - startTime;
+    }
+    try {
+      size = future.get();
+    }catch (InterruptedException | ExecutionException  e) {
+      e.printStackTrace();
+    }
+    executorService.shutdown();
+    return size;
   }
   
   @Override
