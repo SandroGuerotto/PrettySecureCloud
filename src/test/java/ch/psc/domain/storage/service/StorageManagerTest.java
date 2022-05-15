aa3af3b7421a7679a0d423dddd7e79f2906c7acc
walchr01 <walchr01@students.zhaw.ch>
2022-05-16 01:59:29 +0200

    unit test for StorageManager

--- src/test/java/ch/psc/domain/storage/service/StorageManagerTest.java
+++ src/test/java/ch/psc/domain/storage/service/StorageManagerTest.java
@@ -1,6 +1,8 @@
 package ch.psc.domain.storage.service;
 
+import ch.psc.domain.cipher.*;
 import ch.psc.domain.file.PscFile;
+import ch.psc.domain.storage.ProcessEvent;
 import ch.psc.domain.storage.StorageManager;
 import ch.psc.domain.user.User;
 import ch.psc.exceptions.FatalImplementationException;
@@ -8,13 +10,15 @@ import org.bouncycastle.crypto.digests.ParallelHash;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+import org.mockito.*;
+import static org.mockito.Mockito.when;
+
+
 
 import java.io.File;
-import java.math.BigDecimal;
+import java.nio.file.*;
 import java.util.Map;
+import java.util.function.Consumer;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
@@ -28,10 +32,16 @@ import static org.mockito.Mockito.when;
 public class StorageManagerTest {
 
     StorageManager storageManager;
-    FileStorage fileStorage;
     User testUser;
     File testFile;
     PscFile testPscFile;
+    Consumer<ProcessEvent> testConsumer;
+    LocalStorage localStorage;
+    PscCipher cipher;
+    private Map<String, Key> keyChain;
+    File uploadFiles;
+    File downloadFiles;
+    Path testFileDirectory;
 
     @BeforeEach
     void setUp(){
@@ -40,10 +50,28 @@ public class StorageManagerTest {
 
         //user mock
         testUser = Mockito.mock(User.class);
-        testFile = Mockito.mock(File.class);
+        cipher = new AesCipher();
+        ch.psc.domain.cipher.KeyGenerator keyGenerator = new KeyGenerator();
+        try {
+            keyChain = keyGenerator.generateKey(cipher.getKeyBits(), cipher.getAlgorithm());
+        } catch (FatalImplementationException e) {
+            e.printStackTrace();
+        }
+        when(testUser.getKeyChain()).thenReturn(keyChain);
+        when(testUser.getDownloadPath()).thenReturn(testFileDirectory.toFile().getPath()+"\\downloadFiles");
+
+        //testPscFile mock
         testPscFile = Mockito.mock(PscFile.class);
-        fileStorage = Mockito.mock(FileStorage.class);
+        when(testPscFile.getName()).thenReturn("test.txt.psc");
+        when(testPscFile.getPath()).thenReturn(testFileDirectory.toFile().getPath()+"\\test.txt.psc");
+
+
+        //testklasse
         storageManager = new StorageManager(testUser);
+
+        //consumer Mock
+        testConsumer = processEvent -> System.out.println("Consumer activated");
+
     }
 
     @Test
@@ -69,6 +97,25 @@ public class StorageManagerTest {
 
     @Test
     public void testDownloadFiles(){
+        //folder downloadFiles erstellen
+        testFile = new File(testFileDirectory.toFile().getAbsolutePath()+"\\test.txt.psc");
+
+        downloadFiles = new File (testFileDirectory.toFile().getAbsolutePath()+"\\downloadFiles");
+        if (downloadFiles.exists()){
+            deleteDir(downloadFiles);
+        }
+        downloadFiles = new File (testFileDirectory.toFile().getAbsolutePath()+"\\downloadFiles");
+        downloadFiles.mkdirs();
+
+        localStorage = new LocalStorage(downloadFiles.getAbsolutePath());
+        storageManager.downloadFiles(localStorage, testPscFile, testConsumer);
+
+        //check if file exists
+        assertTrue(fileExists(downloadFiles.getAbsolutePath()));
+
+        //delete folder afterwards
+        //deleteDir(downloadFiles);
+
 
     }
 
